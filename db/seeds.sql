12362ca32c99022c5a17812943182441cd232ff0
Doris Chen <dorischen@Doriss-MacBook-Air.local>
2023-05-09 00:03:25 +1000

    add more functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,11 +16,11 @@ VALUES ('Sales Lead',100000,1),
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1,NULL),
+VALUES ('John', 'Doe', 1, Null),
        ('Mike', 'Chan', 2,1),
-       ('Ashley', 'Rod', 3,NULL),
+       ('Ashley', 'Rod', 3,Null),
        ('Kevin', 'Tupik', 4, 3),
-       ('Kunal', 'Singh', 5, NULL),
+       ('Kunal', 'Singh', 5, Null),
        ('Malia', 'Brown', 6, 5),
-       ('Sarah', 'Lourd', 7, NULL),
+       ('Sarah', 'Lourd', 7,Null),
        ('Tom', 'Allen', 8, 7);
